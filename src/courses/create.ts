b8c8307c6d14d0e4e7037a27c9787b5f85581712
kielkow <matheuskiel@fiorifer.com.br>
2022-09-20 11:57:08 -0300

    refact: change columns name

--- src/courses/create.ts
+++ src/courses/create.ts
@@ -3,15 +3,25 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function create() {
-    const result = await prisma.courses.create({
-        data: {
-            name: 'NodeJS',
-            duration: 50,
-            description: 'NodeJS course'
-        }
-    });
+  const result = await prisma.courses.create({
+    data: {
+      name: "Curso de Java",
+      duration: 500,
+      description: "Curso de Java 17",
+      teachers: {
+        connectOrCreate: {
+          where: {
+            name: "Jonh Doe",
+          },
+          create: {
+            name: "Jonh Doe",
+          },
+        },
+      },
+    },
+  });
 
-    console.log(result);
+  console.log(result);
 }
 
 create();
