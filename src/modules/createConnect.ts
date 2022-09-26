e7432c446940d17915a1312605275acafcdaf1a4
kielkow <matheuskiel@fiorifer.com.br>
2022-09-26 11:50:49 -0300

    feat: create module connected

---
+++ src/modules/createConnect.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function createConnect() {
+  const result = await prisma.modules.create({
+    data: {
+      description: "Aprendendo firebase do zero",
+      name: "Aprendendo Firebase",
+      teacherId: '',
+      courseModules: {
+        create: {
+          course: {
+            connect: {
+              id: "e27d03d2-f5ca-4f0c-908e-1d1bdda7a83e",
+            },
+          },
+        },
+      },
+    },
+  });
+
+  console.log(result);
+}
+
+createConnect();
